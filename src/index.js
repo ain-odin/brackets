cf7a743828dcb891e0d4a894522dbc485a388c5b
ain-odin <ain.odintseva@gmail.com>
2023-01-24 13:28:33 +0600

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+/* eslint-disable semi */
+module.exports = function check (str, bracketsConfig) {
+  const arr = str.split('');
+  function deletePairs () {
+    for (let i = 0; i < arr.length; i++) {
+      bracketsConfig.forEach(bracket => {
+        if (bracket[0] === arr[i] && bracket[1] === arr[i + 1]) {
+          arr.splice(i, 2);
+          if (arr.length > 0) {
+            deletePairs();
+          }
+        }
+      });
+    }
+    return arr;
+  }
+  deletePairs();
+  if (arr.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
